2c1cddf1f47b503658c109eeaf69b85a854bd723
Amr alaa ali <92322929+Amr8tom@users.noreply.github.com>
2024-03-19 23:50:42 +0200

    1

---
+++ build/agora_rtc_engine/intermediates/cxx/Debug/n6l5k1g4/logs/x86/build_command_iris_rendering_android.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\amr alaa\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\amr alaa\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\agora_rtc_engine-6.3.0\\android\\.cxx\\Debug\\n6l5k1g4\\x86" ^
+  iris_rendering_android
---
+++ build/agora_rtc_engine/intermediates/cxx/Debug/n6l5k1g4/logs/x86/configure_command.bat
@@ -0,0 +1,18 @@
+@echo off
+"C:\\Users\\amr alaa\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\amr alaa\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\agora_rtc_engine-6.3.0\\android\\src\\main\\cpp" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=16" ^
+  "-DANDROID_PLATFORM=android-16" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\amr alaa\\AppData\\Local\\Android\\sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\amr alaa\\AppData\\Local\\Android\\sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\amr alaa\\AppData\\Local\\Android\\sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\amr alaa\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\Apps\\preno_doctorSide\\build\\agora_rtc_engine\\intermediates\\cxx\\Debug\\n6l5k1g4\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\Apps\\preno_doctorSide\\build\\agora_rtc_engine\\intermediates\\cxx\\Debug\\n6l5k1g4\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\amr alaa\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\agora_rtc_engine-6.3.0\\android\\.cxx\\Debug\\n6l5k1g4\\x86" ^
+  -GNinja
